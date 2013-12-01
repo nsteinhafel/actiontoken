c3aca01acae20936547fabf462f94bc4a5f2c570
Nick Steinhafel <nsteinhafel@gmail.com>
2013-11-30 19:24:42 -0600

    reworked some of the tests, added some coverage, added a todo

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -7,35 +9,30 @@ from actiontoken.models import Token, Rule, Field, Action
 import app_settings
 
 class ActionTokenTests(TestCase): 
-
-  # simple sanity check tests
-
-  def test_token_create_simple(self):
-    token = Token.objects.create(
-      user=User.objects.create(username='TEST'),)
+  def test_token_simple(self):
+    token = Token.objects.create()
     self.assertTrue(token)
     self.assertTrue(token.token)
     self.assertTrue(token.expires)
 
-    print '\nTest Token Creation'
-    print unicode(token)
-
-  def test_rule_create(self):
     token = Token.objects.create(
-      user=User.objects.create(username='TEST'),)
+      user=User.objects.create(username='TEST'),
+      token='asdf',
+      expires=datetime.now(),)
+    self.assertTrue(token)
+    self.assertTrue(token.token)
+    self.assertTrue(token.expires)
+
+  def test_rule_simple(self):
+    token = Token.objects.create()
     rule = Rule.objects.create(
       model='%s.%s' % (User.__module__, User.__name__),
       token=token)
     self.assertTrue(rule)
     self.assertTrue(rule.is_class())
 
-    print '\nTest Rule Creation'
-    print unicode(token)
-    print unicode(rule)
-
-  def test_field_create(self):
-    token = Token.objects.create(
-      user=User.objects.create(username='TEST'),)
+  def test_field_simple(self):
+    token = Token.objects.create()
     rule = Rule.objects.create(
       model='%s.%s' % (User.__module__, User.__name__),
       token=token)
@@ -45,14 +42,9 @@ class ActionTokenTests(TestCase):
     self.assertTrue(field)
     self.assertTrue(field.is_field())
 
-    print '\nTest Field Creation'
-    print unicode(token)
-    print unicode(rule)
-    print unicode(field)
-
   # let's throw in Actions
 
-  def test_rule_action_create(self):
+  def test_rule_action_simple(self):
     token = Token.objects.create(
       user=User.objects.create(username='TEST'),)
     rule = Rule.objects.create(
@@ -65,12 +57,7 @@ class ActionTokenTests(TestCase):
     self.assertTrue(action.has_valid_target())
     self.assertTrue(token.can_read(User))
 
-    print '\nTest Rule-Action (Read) Creation'
-    print unicode(token)
-    print unicode(rule)
-    print unicode(action)
-
-  def test_field_action_create(self):
+  def test_field_action_simple(self):
     token = Token.objects.create(
       user=User.objects.create(username='TEST'),)
     rule = Rule.objects.create(
@@ -81,15 +68,10 @@ class ActionTokenTests(TestCase):
       rule=rule)
     action = Action.objects.create(
       field=field)
+
     self.assertTrue(action.has_valid_action())
     self.assertTrue(action.has_valid_target())
     self.assertTrue(token.can_read(User, 'username'))
-
-    print '\nTest Field-Action (Read) Creation'
-    print unicode(token)
-    print unicode(rule)
-    print unicode(field)
-    print unicode(action)
 
   def test_rule_field_conflict_for_action(self):
     token = Token.objects.create(
@@ -102,6 +84,7 @@ class ActionTokenTests(TestCase):
       rule=rule)
     action = Action.objects.create(
       field=field)
+
     self.assertTrue(action.has_valid_target())
     action.rule = rule
     self.assertFalse(action.has_valid_target())
@@ -109,13 +92,40 @@ class ActionTokenTests(TestCase):
     try:
       action.save()
       self.fail()
-    except InvalidActionTarget:
-      pass
+    except InvalidActionTarget: pass
       
     action.rule = None
     self.assertTrue(action.has_valid_target())
 
-    print '\nTest Rule-Field Conflict for Action'
+  def test_rule_action_create_simple(self):
+    token = Token.objects.create()
+    rule = Rule.objects.create(
+      model='%s.%s' % (User.__module__, User.__name__),
+      token=token)
+    action = Action.objects.create(
+      action=Action.CREATE,
+      rule=rule)
+    self.assertTrue(token.can_create(User))
+
+  # haven't decided how I want the string representations to look, so keeping it simple
+
+  def test_unicode_simple(self):
+    token = Token.objects.create()
+    rule = Rule.objects.create(
+      model='%s.%s' % (User.__module__, User.__name__),
+      token=token)
+    field = Field.objects.create(
+      name='username',
+      rule=rule)
+    action = Action.objects.create(
+      field=field)
+
+    self.assertTrue(len(unicode(token))   > 0)
+    self.assertTrue(len(unicode(rule))    > 0)
+    self.assertTrue(len(unicode(field))   > 0)
+    self.assertTrue(len(unicode(action))  > 0)
+
+    print '\nunicode test'
     print unicode(token)
     print unicode(rule)
     print unicode(field)
